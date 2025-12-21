1041d0dd78ad2c39364b92769b71a5901a143c42
shubham-guptaaa <shubhamguptaa70@gmail.com>
2025-12-21 20:57:56 +0530

    feat: modified types.ts to support X results

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,4 +1,4 @@
-export type Platform = 'youtube' | 'linkedin' | 'instagram' | 'unknown';
+export type Platform = 'youtube' | 'linkedin' | 'instagram' | 'unknown' | 'x';
 
 export interface DeepLinkResult {
   webUrl: string;
