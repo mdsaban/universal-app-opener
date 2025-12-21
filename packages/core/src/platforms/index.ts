3dcc22a13d937dca861ff89368fc4ce8438169f1
shubham-guptaaa <shubhamguptaa70@gmail.com>
2025-12-21 20:58:19 +0530

    feat: updated core index.ts to integrate X into handler list

--- packages/core/src/platforms/index.ts
+++ packages/core/src/platforms/index.ts
@@ -1,11 +1,7 @@
-import { linkedinHandler } from "./linkedin";
-import { unknownHandler } from "./unknown";
-import { youtubeHandler } from "./youtube";
-import { instagramHandler } from "./instagram";
+import { linkedinHandler } from './linkedin';
+import { unknownHandler } from './unknown';
+import { youtubeHandler } from './youtube';
+import { instagramHandler } from './instagram';
+import { xHandler } from './x';
 
-export {
-    linkedinHandler,
-    youtubeHandler,
-    instagramHandler,
-    unknownHandler
-}
+export { linkedinHandler, youtubeHandler, instagramHandler, unknownHandler, xHandler };
