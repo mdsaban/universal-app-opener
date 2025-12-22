5f05505348a91131b260b99c66733451d0a6c8ba
Aarush Sharma <aarushsharma1016@gmail.com>
2025-12-22 19:36:45 +0530

    feat: add twitter support

---
+++ packages/core/src/platforms/twitter.ts
@@ -0,0 +1,16 @@
+import { DeepLinkHandler } from '../types';
+
+export const twitterHandler: DeepLinkHandler = {
+
+  match: (url) => url.match(/twitter\.com\/([^/?]+)/),
+
+  build: (webUrl, match) => {
+    const username = match[1]; 
+    return {
+      webUrl,
+      ios: `twitter://user?screen_name=${username}`,
+      android: `intent://user?screen_name=${username}#Intent;scheme=twitter;package=com.twitter.android;end`,
+      platform: 'twitter'
+    };
+  },
+};
